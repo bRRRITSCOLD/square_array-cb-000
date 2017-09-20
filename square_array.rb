4252664bbce268992c45498ed6d536ab3000b886
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-20 20:00:56 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,13 @@
 def square_array(array)
-  squared = [] 
+  squared = []
   array.each{|element| squared << element ** 2}
   squared
 end
 
-def square_array_collect(array) 
+def square_array_collect(array)
   squared = array.collect{|element| element ** 2}
   squared
 end
+
+lol = [1,2,3,4,5,6,7,8,9]
+square_array_collect(lol)
